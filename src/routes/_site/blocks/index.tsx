aef79107489d8f040791c16167865737fd5f5ec6
phong phan <phongphanq089@gmail.com>
2026-09-24 16:42:13 +0700

    chore: update page block

--- src/routes/_site/blocks/index.tsx
+++ src/routes/_site/blocks/index.tsx
@@ -1,9 +1,28 @@
 import { createFileRoute } from "@tanstack/react-router"
 
+import { BlockGrid } from "@/features/blocks"
+import { createSeoMeta } from "@/shared/config"
+import { IS_PUBLIC_UI } from "@/shared/constants"
+import { UnderConstructionBlock } from "@/shared/ui"
+
 export const Route = createFileRoute("/_site/blocks/")({
-  component: RouteComponent,
+  head: () => ({
+    meta: createSeoMeta("blocks"),
+  }),
+  component: BlocksAllPage,
 })
 
-function RouteComponent() {
-  return <div>Hello "/_site/blocks/"!</div>
+function BlocksAllPage() {
+  return (
+    <div className="w-full">
+      {IS_PUBLIC_UI ? (
+        <BlockGrid category="all" />
+      ) : (
+        <UnderConstructionBlock
+          moduleName="Engineering Block ui"
+          moduleBadge="BLOCK"
+        />
+      )}
+    </div>
+  )
 }
